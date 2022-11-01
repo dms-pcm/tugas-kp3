8ed0c61da824c4c8cc8b25a31f24ddc8ead0c4a3
dms-pcm <81421412+dms-pcm@users.noreply.github.com>
2022-11-01 15:49:43 +0700

    updatee

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1417,7 +1417,7 @@
                     <div class="row gy-6">
                         <div class="col-12">
                             <div class="avatar avatar-xl mx-auto">
-                                <img src="{{ asset('storage/avators/' . $current->avator) }}" alt="{{ $current->full_name}}" class="avatar-img" style="border:2px solid #ffffff">
+                                <img src="{{ asset('storage/avators/' . $current->avator) }}" alt="{{ $current->full_name}}" class="avatar-img bunder">
                                 <!-- <a class="badge badge-lg badge-circle bg-primary text-white border-outline position-absolute bottom-0 end-0">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
                                 </a> -->
